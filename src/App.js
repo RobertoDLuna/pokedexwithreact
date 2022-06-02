8066940f83329831e00f9738f1bb40f1966c47c0
Roberto Luna <robertocgw@gmail.com>
2022-06-02 18:49:20 -0300

    Adição da barra de pesquisa e do botão de pesquisar o pokemon

--- src/App.js
+++ src/App.js
@@ -3,30 +3,21 @@ import { Switch, Route } from "react-router-dom";
 import Informacoes from './router/Informacoes';
 import Favoritos from "./router/Favoritos";
 import NotFound from "./router/NotFound";
-import Rotas from './router/Rotas';
 import Header from './components/Header';
 import './App.css';
-import { buscaPorNome, buscaTodosOsPokemon, buscaPorTipo } from './services/api';
+import Inicio from "./router/Inicio";
 
 export default class App extends React.Component {
-  async componentDidMount() {
-    const ditto = await buscaPorNome('ditto');
-    console.log(ditto);
-    const todos = await buscaTodosOsPokemon();
-    console.log(todos);
-    const normal = await buscaPorTipo('normal');
-    console.log(normal);
-  }
   render() {
     return (
       <div>
         <Header />
-				<Switch>
-          <Route exact path="/" component={ Rotas } />
-					<Route exact path="/informacoes/:id" component={Informacoes} />
-					<Route exact path="/favoritos" component={Favoritos} />
-					<Route path="*" component={NotFound} />
-				</Switch>
+        <Switch>
+          <Route exact path="/" component={Inicio} />
+          <Route exact path="/informacoes/:id" component={Informacoes} />
+          <Route exact path="/favoritos" component={Favoritos} />
+          <Route path="*" component={NotFound} />
+        </Switch>
       </div>
     );
   }
